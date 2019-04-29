183bfff08b9867cb7874450519d85ba9163ff31e
Joe A <jasercion.gwu@gmail.com>
2019-04-29 02:42:53 +0000

    Updates to server

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,27 @@
-extern crate iron;
-extern crate urlencoded;
-extern crate router;
-extern crate serde;
-#[macro_use] extern crate mime;
-
-use iron::prelude::*;
-use iron::status;
-use router::Router;
-use std::str::FromStr;
-use urlencoded::UrlEncodedBody;
-use serde::{Serialize, Deserialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Poll {
-    title: String,
-    options: &mut Vec<String>,
-    multivote: bool,
-    delete: bool,
-}
-
-struct Ballot {
-    username: String,
-    selection: uint,
-    poll_id: uint,
-}
-
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+use serde_urlencoded::Deserialize;
 
 fn main() {
-    println!("Starting Slack Polling server on http://localhost:3000...");
-    
-    let mut router = Router::new();
+    let listener = TcpListener::bind("10.0.0.4:7878").unwrap();
 
-    router.get("/", handle_request, "root");
-    router.post("/", post_response, "post");
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
 
-    Iron::new(router).http("localhost:3000").unwrap();
+        handle_connection(stream);
+    }
 }
 
-fn handle_request(_request: &mut Request) -> IronResult<Response> {
-   
-};
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
 
+    stream.read(&mut buffer).unwrap();
 
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+    
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}

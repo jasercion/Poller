c8466a69837d76d69dbcf31cefb8429bf69d7d98
Joe A <jasercion.gwu@gmail.com>
2019-04-30 18:16:38 +0000

    Add struct to hold request data

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
+extern crate url;
+
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
-use serde_urlencoded::Deserialize;
+use url::form_urlencoded;
+
+
+
+// # Request Struct
+//
+// Created to hold useful infomation
+// extracted from the incoming TcpListener
+// stream.  `command` refers specifically to
+// the slash command which invoked the app while
+// `text` holds any information included after
+// the command input.  The remainder of the
+// information is metadata populated by Slack.
+//
+
+struct Request {
+    command: String,
+    text: String,
+    response_url: String,
+    user_id: String,
+}
+
 
 fn main() {
     let listener = TcpListener::bind("10.0.0.4:7878").unwrap();
@@ -14,14 +37,27 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
+    let mut buffer = Vec::new();
+    let mut request = Request {
+        command: String::new(),
+        text: String::new(),
+        response_url: String::new(),
+        user_id: String::new(),
+    };
 
-    stream.read(&mut buffer).unwrap();
+    stream.read_to_end(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    let post = form_urlencoded::parse(&buffer);
     
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    for val in post {
+        match val.0.as_ref() {
+            "command" => request.command = val.1.to_string(),
+            "text" => request.text = val.1.to_string(),
+            "response_url" => request.response_url = val.1.to_string(),
+            "user_id" => request.user_id = val.1.to_string(),
+            _ => println!("No match for {}", val.0),
+        };
+        
+        println!("({:?}, {:?})\n", val.0, val.1);
+    }
 }
